95682fb0318014bcaf78fceceb660cb6befe3d2e
thakur-Yuvraj <yuvipatel224@gmail.com>
2025-03-06 11:51:39 +0530

    [Yuvraj Thakur] Add. site link added to the image

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -10,9 +10,15 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'Helloworld';
 
-  imgUrl = "../assets/BL_logo_square_jpg.jpg";
+  url = "https://www.bridgelabz.com";
+  imgUrl = "https://i.pinimg.com/736x/71/d9/a9/71d9a9a5614ec8a9a17079ace15f10a0.jpg";
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.title = "Hello from brigdeLabz ! ";
+  }
+
+  onClick($event: any) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }
